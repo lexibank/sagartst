6933af1624fb239084d2c82a1aa42e2facc78138
lingulist <mattis.list@lingulist.de>
2018-10-18 09:35:30 +0200

    update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('metadata.json', **({} if PY2 else {'encoding': 'utf-8'})) as fp:
 
 setup(
     name='lexibank_sagartst',
-    version="1.0.0",
+    version="0.9.0",
     description=metadata['title'],
     license=metadata.get('license', ''),
     url=metadata.get('url', ''),
